4bee3db29fdf9c7be5d20db850f5704d06bc060d
kaleb <121309809+starbouns@users.noreply.github.com>
2023-10-07 20:08:07 +0300

    again

--- config.js
+++ config.js
@@ -2,3 +2,10 @@ console.log('first commit')
 console.log('version 2')
 console.log('version 3 config')
 console.log('version 13')
+function generatePowerOfTwo(n) {
+    let result = [];
+    for (let i = 0; i <= n; i++) {
+        result.push(Math.pow(2, i));
+    }
+    return result;
+}
